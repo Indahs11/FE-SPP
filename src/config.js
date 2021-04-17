7169c0cea6e2224c5771474fc92d1e1d6a67f454
Indahs11 <indahsrw11@gmail.com>
2021-04-17 21:08:02 +0700

    frontend-pembyaran-spp

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const base_url = "http://localhost:8000/store/api/v1"
+const foto_siswa_url = "http://localhost:8000/foto_siswa/"
+const foto_petugas_url = "http://localhost:8000/foto_petugas/"
+export {
+    base_url,
+    foto_siswa_url,
+    foto_petugas_url
+}
